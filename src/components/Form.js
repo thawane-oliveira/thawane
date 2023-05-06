ae5d4721648ac89b526ea4e6db03b04434741142
Thawane <thawaneapoliveira@gmail.com>
2023-05-05 23:46:58 -0300

    feat: adiciona controle de estado do botão, ficando desativado em caso de algum campo de contato inválido

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import check from '../images/checkmark.png';
 
@@ -9,6 +9,8 @@ const publicKey = process.env.REACT_APP_YOUR_PUBLIC_KEY;
 function Form() {
   const form = useRef();
   const [isSend, setIsSend] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [input, setInput] = useState({ name: '', email: '', message: '' });
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -22,26 +24,50 @@ function Form() {
       });
   };
 
+  const handleChange = (e) => {
+    console.log(e.target.id, e.target.value);
+    const inputName = e.target.id.split('_')[1];
+    setInput({ ...input, [inputName]: e.target.value })
+  }
+
+  useEffect(() => {
+    const verifyFields = () => {
+      const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}/;
+      // console.log(input.name.length > 3, regex.test(input.email), input.message.length > 5)
+      if (input.name.length > 3
+        && regex.test(input.email)
+        && input.message.length > 5) {
+        console.log('passou');
+        setIsDisabled(false)
+      } else {
+        console.log('deu ruim');
+        setIsDisabled(true)
+      }
+
+    };
+    verifyFields();
+  });
+
   return (
-    isSend ? 
-    <>
-    <h1 className="sm:text-md lg:text-lg md:text-lg text-violet-900 text-center">
-      Obrigada pela mensagem.
-      <br/>
-      Responderei o mais breve possível.
-    </h1>
-    <img src={check} alt="Símbolo de check, que indica que o e-mail foi enviado com sucesso." className='h-2/5 md:w-1/4 lg:w-15 sm:w-1/5 mx-auto'/>
-    </>
+    isSend ?
+      <>
+        <h1 className="sm:text-md lg:text-lg md:text-lg text-violet-900 text-center">
+          Obrigada pela mensagem.
+          <br />
+          Responderei o mais breve possível.
+        </h1>
+        <img src={check} alt="Símbolo de check, que indica que o e-mail foi enviado com sucesso." className='h-2/5 md:w-1/4 lg:w-15 sm:w-1/5 mx-auto' />
+      </>
       : (
-      <form ref={form} onSubmit={sendEmail} className='w-full h-full flex flex-col justify-around items-center'>
-        <label htmlFor="user_name" className="text-center">Nome</label>
-        <input type="text" name="user_name" id="user_name" className="w-3/4 px-1 rounded-md text-center"/>
-        <label htmlFor="user_email" className="text-center">E-mail</label>
-        <input type="email" name="user_email" id="user_email" className="w-3/4 px-1 rounded-md text-center"/>
-        <label htmlFor="message" className="text-center">Mensagem</label>
-        <textarea name="message" id="message"  className="w-3/4 h-2/6 px-1 rounded-md"/>
-        <input type="submit" value="Enviar" className="semibold bg-violet-400 w-2/5 rounded-md text-stone-50 cursor-pointer px-2"/>
-      </form>
+        <form ref={form} onSubmit={sendEmail} className='w-full h-full flex flex-col justify-around items-center'>
+          <label htmlFor="user_name" className="text-center">Nome</label>
+          <input type="text" name="user_name" id="user_name" className="w-3/4 px-1 rounded-md text-center bgrad" onChange={handleChange} value={input.name} />
+          <label htmlFor="user_email" className="text-center">E-mail</label>
+          <input type="email" name="user_email" id="user_email" className="w-3/4 px-1 rounded-md text-center bgrad" onChange={handleChange} value={input.email} />
+          <label htmlFor="user_message" className="text-center">Mensagem</label>
+          <textarea name="message" id="user_message" className="w-3/4 h-2/6 px-1 rounded-md bgrad" onChange={handleChange} value={input.message} />
+          <input disabled={isDisabled} type="submit" value="Enviar" className="semibold bg-violet-400 w-2/5 rounded-md text-stone-50 cursor-pointer px-2" />
+        </form>
       )
   );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -6,14 +6,12 @@ function About() {
     <>
       <div className='h-screen w-screen overflow-auto flex items-center flex-col justify-between container mx-auto'>
         <Header />
-        <div className='flex flex-col justify-around items-center bg-gradient-to-r from-violet-200 to-yellow-50 h-2/4 w-4/5 shadow-lg shadow-violet-400 py-4 px-6 mx-4 rounded-md'>
-          <h1 className='text-3xl text-violet-900 font-semibold'>Sobre mim</h1>
-          <p className='h-fit overflow-auto text-justify w-4/5 leading-8'>
-            Olá, meu nome é Thawane, tenho 26 anos, atualmente moro em São Paulo, mas sou natural de São Vicente, litoral de SP. Sou inicialmente formada em Design de Moda. Sou desenvolvedora front-end e em breve, full stack.
+        <div className='flex flex-col items-center bg-gradient-to-r from-violet-200 to-yellow-50 h-3/5 w-4/5 shadow-lg shadow-violet-400 py-4 px-6 mx-4 rounded-md'>
+          <h1 className='text-3xl h-1/4 text-violet-900 font-semibold'>Sobre mim</h1>
+          <p className='h-3/4 overflow-auto text-justify w-4/5 leading-8'>
+            Olá, meu nome é Thawane e sou uma desenvolvedora front-end apaixonada por design e usabilidade voltado ao usuário. Atualmente moro em São Paulo, mas sou natural de São Vicente, litoral de SP. Me formei inicialmente em Design de Moda e fiz a transição de carreira da área administrativa em uma prefeitura municipal para me dedicar ao desenvolvimento web. Estou estudando na Trybe e agora estou no módulo de back-end, mas já concluí os módulos de fundamentos e front-end, nos quais desenvolvi mais de 20 projetos, tanto individualmente quanto em equipe.
             <br />
-            Sempre tive um grande interesse em tecnologia. Desde criança, era fascinada com os bichinhos virtuais e como eles funcionavam. Eu também tinha curiosidade sobre a arte em pixels era contida neles e como ela era criada.
-            <br />
-            Estou em transição de carreira e venho da área administrativa (exercida em prefeitura municipal). Estudo desenvolvimento web na Trybe. Atualmente, estou no módulo de back-end, já fiz feitos mais de 20 projetos nos módulos anteriores: fundamentos e front-end, tanto individuais quanto em grupo.
+            Tenho grande interesse em tecnologia desde criança, quando me fascinava com os bichinhos virtuais e a arte em pixels que os compunham, despertando a minha curiosidade sobre como eles funcionavam e eram criados. Acredito que a união de um bom design com uma ótima experiência do usuário é essencial para o sucesso de um projeto web. Estou sempre em busca de aprimorar minhas habilidades e explorar novas tecnologias para desenvolver projetos incríveis que atendam às necessidades dos usuários.
           </p>
         </div>
         <Footer />
@@ -21,5 +19,4 @@ function About() {
     </>
   );
 }
-//Acesse meu curriculo
 export default About;
